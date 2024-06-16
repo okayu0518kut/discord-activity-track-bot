31b41d062335e802bef0b54ed82cb7ac5b93e282
abc <250376s@ugs.kochi-tech.ac.jp>
2024-06-16 20:00:28 +0900

    サーバにログを残すように変更

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const client = new Discord.Client({
   ]
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+const BOT_CHANNEL_ID = process.env.BOT_CHANNEL_ID;
+
+client.login(DISCORD_BOT_TOKEN);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -22,8 +25,20 @@ async function getActivity(userId) {
   return new Promise((resolve, reject) => {
     client.guilds.cache.forEach(async (guild) => {
       const member = await guild.members.fetch(userId);
+	    act = member.presence.activities
       if (member) {
-        resolve(member.presence.activities);
+        resolve(act);
+
+    // discordサーバにメッセージ送信
+    const channel = client.channels.cache.get(BOT_CHANNEL_ID)
+    if (channel && act.length > 0){
+	    for (let ii=0;ii<act.length;++ii)
+	    channel.send(
+		    "name: " + act[ii].name + "\n" +
+		    "state: " + act[ii].state + "\n" +
+		    "details: " + act[ii].details + "\n"
+	    )
+    }
       } else {
         reject(new Error('User not found'));
       }
